fffbf5984d71d4dbfa3cce6d2a47eac6cdad6127
Mike Nicholaides <mike@nicholaides.com>
2023-09-22 06:44:51 -0400

    Scaffold gem

---
+++ lib/cecil.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "cecil/version"
+
+module Cecil
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/cecil/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Cecil
+  VERSION = "0.1.0"
+end
