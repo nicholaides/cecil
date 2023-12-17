507d0581fbe52fc0957073656c6f7f76a767798e
Mike Nicholaides <mike@nicholaides.com>
2023-12-17 12:36:28 -0500

    Add more comments

--- lib/cecil.rb
+++ lib/cecil.rb
@@ -6,7 +6,9 @@ require_relative "cecil/syntax"
 module Cecil
   class Code < Syntax
     class << self
-      def call(out = $stdout, &)
+      # Generates output and sends it to `out` via `<<`. `out` defaults to `$stdout`,
+      # but can be any object that responds to `<<`.
+      def generate(out = $stdout, &)
         syntax = new
         builder = Builder.new(syntax)
         BlockContext.new(builder, syntax.helpers).instance_exec(&)
@@ -17,12 +19,29 @@ module Cecil
           .lstrip
           .then { out << _1 }
       end
+      alias call generate
 
-      def generate_string(&) = call("", &)
+      # Generates output and returns it as a string
+      def generate_string(&) = generate("", &)
 
+      # Define (or return) helpers for use inside the Cecil block. Each subclass
+      # of `Code` has its own helpers.
+      #
+      # E.g.
+      #   class HTML < Cecil::Code
+      #     helpers do
+      #       def h(str) = CGI.escape(str)
+      #     end
+      #   end
+      #
+      #   page = "My Geocities Site ~~< >~~"
+      #   HTML.generate_string do
+      #     `<title>$page</title>`[h page]
+      #   end
+      # If given a block, calls the block inside a new Module and returns the module.
       def helpers(&)
-        @helpers = Module.new(&) if block_given?
         @helpers ||= Module.new
+        @helpers.module_exec(&) if block_given?
         @helpers
       end
     end
