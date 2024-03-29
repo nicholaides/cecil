037123480ef53799092113eb32c65bd97937b0cc
Mike Nicholaides <mike@nicholaides.com>
2024-03-29 15:09:47 -0700

    Remove unused test helpers

--- spec/helpers/cecil_helpers.rb
+++ spec/helpers/cecil_helpers.rb
@@ -3,14 +3,4 @@ require "cecil"
 module CecilHelpers
   def expect_code(...) = expect(code(...))
   def code(...) = Cecil::Code.generate_string(...)
-
-  def cecil(&block)
-    @cecil_block = block
-  end
-
-  def eval_cecil_block = code(&@cecil_block)
-
-  def outputs(...) = expect(eval_cecil_block).to eq(...)
-
-  def raises(...) = expect { eval_cecil_block }.to raise_error(...)
 end
